3a140f6d151782bd1c6402730cd011a8eb191738
yu-zyu <siragaigarasi@gmail.com>
2021-10-26 21:00:07 +0900

    fix

--- SearchAlgorithm/BinaryTree/python/BinaryTree.py
+++ SearchAlgorithm/BinaryTree/python/BinaryTree.py
@@ -1,7 +1,7 @@
 def search(data, target):
     start, end = 0, len(data)-1 #探索するデータの始点startと終点endを設定
     while start <= end: #探索するデータがある間は繰り返す
-        i = (start + end) //2 #真ん中のデータをiとする
+        i = (start + end) // 2 #真ん中のデータをiとする
         if data[i] == target: #見つかったときにはその位置iを返す
             return i
         elif data[i] < target: #targetの値の方が大きい場合は後のグループを探索
